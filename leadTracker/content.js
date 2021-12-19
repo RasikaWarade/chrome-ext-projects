cf5af9919a649c0f1776825b213a570dee051365
Rasika Warade <rwarade@Rasikas-MBP.lan>
2021-12-19 13:43:40 -0500

    fetch get and post sample

--- leadTracker/content.js
+++ leadTracker/content.js
@@ -6,6 +6,21 @@ console.log(currDoc)
 let jsonparsed = JSON.parse(currDoc[0].innerText)
 console.log(jsonparsed)
 
+fetch('https://reqres.in/api/products/3')
+.then(response => response.text())
+.then(response => console.log(response))
+
+
+fetch('https://jobbaby.xyz/api', {
+  method: 'POST',
+  body: JSON.stringify(jsonparsed),
+  mode: 'no-cors'
+})
+.then(response => response.text())
+.then(result => {
+  console.log('Success:', result);
+})
+
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
         console.log("gotMessage")
         sendResponse({
